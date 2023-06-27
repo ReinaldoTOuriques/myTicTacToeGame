25b81f6e0d1a9c2c70afd6cd752add4dd344bfb2
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-06-27 12:51:16 -0300

    chore: Cria o método de fazer jogadas utilizando a posição do jogador atual no tabuleiro e alterna para o próximo.

--- myTicTacToeGame.js
+++ myTicTacToeGame.js
@@ -6,6 +6,14 @@ class TicTacToe {
     // Define o jogador atual como 'X'.
     this.player = 'X'
   }
+
+  // Método para fazer uma jogada.
+  play (index) {
+    // Atualiza a posição no tabuleiro com a marca do jogador atual.
+    this.board[index] = this.player
+    // Alterna o jogador atual para o próximo.
+    this.player = this.player === 'X' ? 'O' : 'X'
+  }
 }
 
 // Exporta a classe TicTacToe para uso em outros arquivos.
