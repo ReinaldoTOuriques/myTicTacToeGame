f96613f5e994aeeff734c2bd9966fdb77a0f6ad1
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-06-27 12:36:30 -0300

    test: Cria teste onde o jogador deve ser capaz de fazer uma jogada e alternar o jogador atual após a jogada.

--- myTicTacToeGame.test.js
+++ myTicTacToeGame.test.js
@@ -21,4 +21,21 @@ describe('TicTacToe', () => {
     // Verifica se o jogador do jogo é igual a 'X'.
     expect(game.player).toEqual('X')
   })
+
+  // Teste: o jogador deve ser capaz de fazer uma jogada e alternar o jogador atual após a jogada.
+  test('should be able to make a play and alternate the current player', () => {
+    // Faz uma jogada na posição 0.
+    game.play(0)
+    // Verifica se a posição 0 do tabuleiro é igual a 'X'.
+    expect(game.board[0]).toEqual('X')
+    // Verifica se o jogador atual é igual a 'O'.
+    expect(game.player).toEqual('O')
+
+    // Faz uma jogada na posição 1.
+    game.play(1)
+    // Verifica se a posição 1 do tabuleiro é igual a 'O'.
+    expect(game.board[1]).toEqual('O')
+    // Verifica se o jogador atual é igual a 'X'.
+    expect(game.player).toEqual('X')
+  })
 })
