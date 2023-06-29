a1525dbe52c78318e51e6ed601175578c1ce9d8b
Reinaldo Takashima Ouriques <reinaldotouriques@gmail.com>
2023-06-28 21:17:49 -0300

    test: Refatorei o teste pois, da forma como estava escrito anteriormente, a lógica estava incorreta. Agora, reescrevendo quem é o currentePlayer esperado após a última jogada feita pelo teste, ele passa a refletir de forma correta quem é o jogador atual. Como estava antes, o teste esperava que o currentPlayer fosse o jogador 'X' quando, na verdade, deveria ser o jogador 'O'. Além disso mas não menos importante, dentro do método constructor, elenquei todas as combinações vencedoras possíveis e, dessa forma, desenvolvi os métodos responsáveis por verificar se o jogo acabou em empate ou se houve um vencedor.

--- javascripts/myTicTacToeGame.js
+++ javascripts/myTicTacToeGame.js
@@ -15,6 +15,20 @@ class TicTacToe {
     this.currentPlayerName = () => {
       return this.currentPlayer === playerX ? this.playerX : this.playerO
     }
+    // Define as combinações de vitória.
+    this.winningCombinations = [
+      // Combinações horizontais.
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // Combinações verticais.
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // Combinações diagonais.
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
   }
 
   // Método para fazer uma jogada.
@@ -29,6 +43,31 @@ class TicTacToe {
     this.board[index] = this.currentPlayer
     // Alterna o jogador atual para o próximo.
     this.currentPlayer = this.currentPlayer === playerX ? playerO : playerX
+  }
+
+  // Método para verificar se o jogo acabou com vitória ou empate.
+  isGameOver () {
+  // Verifica se todas as células estão preenchidas (empate) ou se há um vencedor.
+    return this.board.every(cell => cell !== null) || this.getWinner() !== null
+  }
+
+  // Método para obter o vencedor do jogo.
+  getWinner () {
+    // Percorre todas as combinações vencedoras possíveis.
+    for (const combination of this.winningCombinations) {
+      const [a, b, c] = combination
+      // Verifica se as células da combinação atual possuem o mesmo valor (X ou O).
+      if (
+        this.board[a] &&
+      this.board[a] === this.board[b] &&
+      this.board[a] === this.board[c]
+      ) {
+      // Retorna o valor do jogador vencedor (X ou O).
+        return this.board[a]
+      }
+    }
+    // Caso não haja um vencedor, retorna null.
+    return null
   }
 }
 
